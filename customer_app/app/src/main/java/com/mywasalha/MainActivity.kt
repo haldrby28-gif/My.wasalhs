ff8ebbf291a82d856f8ca6fe4518faf10e92b47d
haldrby28-gif <haldrby28@gmail.com>
2026-07-19 02:36:18 +0300

    Update MainActivity.kt

--- customer_app/app/src/main/java/com/mywasalha/MainActivity.kt
+++ customer_app/app/src/main/java/com/mywasalha/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mywasalha
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val restaurants = findViewById<Button>(R.id.restaurantsBtn)
+        val shops = findViewById<Button>(R.id.shopsBtn)
+        val pharmacy = findViewById<Button>(R.id.pharmacyBtn)
+        val orders = findViewById<Button>(R.id.ordersBtn)
+
+
+        restaurants.setOnClickListener {
+            Toast.makeText(this, "قسم المطاعم", Toast.LENGTH_SHORT).show()
+        }
+
+        shops.setOnClickListener {
+            Toast.makeText(this, "قسم المحلات", Toast.LENGTH_SHORT).show()
+        }
+
+        pharmacy.setOnClickListener {
+            Toast.makeText(this, "قسم الصيدليات", Toast.LENGTH_SHORT).show()
+        }
+
+        orders.setOnClickListener {
+            Toast.makeText(this, "قسم طلباتي", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
