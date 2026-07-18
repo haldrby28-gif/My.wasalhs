3a2406e1e47854a69684fc74308c860e35936ea4
haldrby28-gif <haldrby28@gmail.com>
2026-07-18 23:18:12 +0300

    Update Cart.kt

--- customer_app/models/Cart.kt
+++ customer_app/models/Cart.kt
@@ -1,1 +1,14 @@
 
+package com.mywasalha.models
+
+data class Cart(
+    val id: String = "",
+    val userId: String = "",
+    val productId: String = "",
+    val productName: String = "",
+    val productImage: String = "",
+    val quantity: Int = 1,
+    val price: Double = 0.0,
+    val totalPrice: Double = 0.0,
+    val restaurantId: String = ""
+)
