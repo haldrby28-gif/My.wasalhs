59dc30ebe16cbfe7d7d3fc8fa7664dbb93260be0
haldrby28-gif <haldrby28@gmail.com>
2026-07-18 23:23:20 +0300

    Update Category.kt

--- customer_app/models/Category.kt
+++ customer_app/models/Category.kt
@@ -1,1 +1,10 @@
 
+package com.mywasalha.models
+
+data class Category(
+    val id: String = "",
+    val name: String = "",
+    val imageUrl: String = "",
+    val description: String = "",
+    val isActive: Boolean = true
+)
