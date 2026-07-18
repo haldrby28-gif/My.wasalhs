05e2b19e728dc7ee98dc871f2ea83d3ef3862a55
haldrby28-gif <haldrby28@gmail.com>
2026-07-18 23:22:14 +0300

    Update User.kt

--- customer_app/models/User.kt
+++ customer_app/models/User.kt
@@ -1,1 +1,15 @@
+package com.mywasalha.models
 
+data class User(
+    val id: String = "",
+    val fullName: String = "",
+    val phone: String = "",
+    val email: String = "",
+    val address: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val profileImage: String = "",
+    val userType: String = "customer",
+    val isActive: Boolean = true,
+    val createdAt: Long = System.currentTimeMillis()
+)
