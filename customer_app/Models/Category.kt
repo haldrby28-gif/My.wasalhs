8637742a7a19c2f33453d8fb0572512f9941629e
haldrby28-gif <haldrby28@gmail.com>
2026-07-18 23:23:35 +0300

    Delete customer_app/Models directory

--- customer_app/Models/Category.kt
+++
@@ -1,9 +0,0 @@
-package com.mywasalha.models
-
-data class Category(
-    val id: String = "",
-    val name: String = "",
-    val imageUrl: String = "",
-    val description: String = "",
-    val isActive: Boolean = true
-)
