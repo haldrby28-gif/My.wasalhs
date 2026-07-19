cebb62979ba5441515fc5edf9a54d060c9e0ca8d
haldrby28-gif <haldrby28@gmail.com>
2026-07-19 03:49:54 +0300

    Create Product.kt

---
+++ customer_app/app/src/main/java/com/mywasalha/models/Product.kt
@@ -0,0 +1,10 @@
+package com.mywasalha.models
+
+data class Product(
+    val id: String = "",
+    val storeId: String = "",
+    val name: String = "",
+    val description: String = "",
+    val image: String = "",
+    val price: Double = 0.0
+)
