de087428be52d73efbb38d66feb82f45cdb38dd6
haldrby28-gif <haldrby28@gmail.com>
2026-07-19 03:42:13 +0300

    Create Restaurant.kt

---
+++ customer_app/app/src/main/java/com/mywasalha/models/Restaurant.kt
@@ -0,0 +1,10 @@
+package com.mywasalha.models
+
+data class Restaurant(
+    val id: String = "",
+    val name: String = "",
+    val image: String = "",
+    val category: String = "",
+    val address: String = "",
+    val phone: String = ""
+)
