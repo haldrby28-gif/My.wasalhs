30a71cc52ea2e313786c5afffa4a837e865b5ffc
haldrby28-gif <haldrby28@gmail.com>
2026-07-22 01:08:47 +0300

    Create Order.kt

---
+++ admin_app/app/src/main/java/com/waselha/admin/Order.kt
@@ -0,0 +1,31 @@
+package com.waselha.admin
+
+data class Order(
+
+    var id: String = "",
+
+    var orderNumber: String = "",
+
+    var customerId: String = "",
+    var customerName: String = "",
+    var customerPhone: String = "",
+
+    var restaurantId: String = "",
+    var restaurantName: String = "",
+
+    var driverId: String = "",
+    var driverName: String = "",
+
+    var totalPrice: Double = 0.0,
+
+    var paymentMethod: String = "",
+
+    var orderStatus: String = "NEW",
+
+    var deliveryAddress: String = "",
+
+    var notes: String = "",
+
+    var createdAt: Long = 0
+
+)
