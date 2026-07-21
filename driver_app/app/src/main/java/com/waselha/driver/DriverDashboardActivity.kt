20addd796b93b4bd803b02243309decad5f3fa61
haldrby28-gif <haldrby28@gmail.com>
2026-07-22 02:15:34 +0300

    Update DriverDashboardActivity.kt

--- driver_app/app/src/main/java/com/waselha/driver/DriverDashboardActivity.kt
+++ driver_app/app/src/main/java/com/waselha/driver/DriverDashboardActivity.kt
@@ -1,48 +1,44 @@
-package com.waselha.driver
+package com.mywasalha.driver
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+
 
 class DriverDashboardActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+
+    private lateinit var availableOrdersButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_driver_dashboard)
 
-        auth = FirebaseAuth.getInstance()
 
-        val btnAvailableOrders = findViewById<Button>(R.id.btnAvailableOrders)
-        val btnCurrentOrder = findViewById<Button>(R.id.btnCurrentOrder)
-        val btnEarnings = findViewById<Button>(R.id.btnEarnings)
-        val btnProfile = findViewById<Button>(R.id.btnProfile)
-        val btnLogout = findViewById<Button>(R.id.btnLogout)
 
-        btnAvailableOrders.setOnClickListener {
-            startActivity(Intent(this, AvailableOrdersActivity::class.java))
+        availableOrdersButton =
+            findViewById(R.id.btnAvailableOrders)
+
+
+
+        availableOrdersButton.setOnClickListener {
+
+
+            val intent =
+                Intent(
+                    this,
+                    AvailableOrdersActivity::class.java
+                )
+
+
+            startActivity(intent)
+
         }
 
-        btnCurrentOrder.setOnClickListener {
-            startActivity(Intent(this, CurrentOrderActivity::class.java))
-        }
-
-        btnEarnings.setOnClickListener {
-            startActivity(Intent(this, EarningsActivity::class.java))
-        }
-
-        btnProfile.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
-        }
-
-        btnLogout.setOnClickListener {
-            auth.signOut()
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }
     }
+
 }
