f2d4ed3b51bec3b7a3f7816d55288e97646da93b
haldrby28-gif <haldrby28@gmail.com>
2026-07-22 03:07:24 +0300

    Update CartManager.kt

--- customer_app/app/src/main/java/com.mywasalha/firebase/CartManager.kt
+++ customer_app/app/src/main/java/com.mywasalha/firebase/CartManager.kt
@@ -1,1 +1,98 @@
 
+package com.mywasalha.firebase
+
+
+import com.mywasalha.models.CartItem
+
+
+
+object CartManager {
+
+
+    private val cartItems =
+        mutableListOf<CartItem>()
+
+
+
+    fun addItem(item: CartItem) {
+
+
+        val existingItem =
+            cartItems.find {
+                it.id == item.id
+            }
+
+
+
+        if (existingItem != null) {
+
+
+            existingItem.quantity += 1
+
+
+        } else {
+
+
+            cartItems.add(item)
+
+        }
+
+    }
+
+
+
+
+    fun removeItem(itemId: String) {
+
+
+        cartItems.removeAll {
+
+            it.id == itemId
+
+        }
+
+    }
+
+
+
+
+    fun getItems(): MutableList<CartItem> {
+
+
+        return cartItems
+
+    }
+
+
+
+
+    fun getTotal(): Double {
+
+
+        var total = 0.0
+
+
+        for (item in cartItems) {
+
+
+            total +=
+                item.price * item.quantity
+
+        }
+
+
+        return total
+
+    }
+
+
+
+
+    fun clearCart() {
+
+
+        cartItems.clear()
+
+    }
+
+}
