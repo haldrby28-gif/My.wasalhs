484c583fee697210ce4a0dffc522a3a9ad3647cf
haldrby28-gif <haldrby28@gmail.com>
2026-07-22 03:01:03 +0300

    Update Product.kt

--- customer_app/app/src/main/java/com.mywasalha/models/Product.kt
+++ customer_app/app/src/main/java/com.mywasalha/models/Product.kt
@@ -1,1 +1,17 @@
 
+package com.mywasalha.models
+
+
+data class Product(
+
+    var id: String = "",
+
+    var name: String = "",
+
+    var description: String = "",
+
+    var price: Double = 0.0,
+
+    var restaurantId: String = ""
+
+)
