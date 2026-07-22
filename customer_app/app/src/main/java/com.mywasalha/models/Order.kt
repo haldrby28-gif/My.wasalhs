b6a8b54559c14c82fe75c26a203400c3b8aca1b6
haldrby28-gif <haldrby28@gmail.com>
2026-07-22 15:47:52 +0300

    Update Order.kt

--- customer_app/app/src/main/java/com.mywasalha/models/Order.kt
+++ customer_app/app/src/main/java/com.mywasalha/models/Order.kt
@@ -1,1 +1,23 @@
+package com.mywasalha.models
 
+data class Order(
+
+    var id: String = "",
+
+    var customerId: String = "",
+
+    var restaurantId: String = "",
+
+    var address: String = "",
+
+    var totalPrice: Double = 0.0,
+
+    var status: String = "available",
+
+    var driverId: String = "",
+
+    var createdAt: Long = 0,
+
+    var items: List<CartItem> = emptyList()
+
+)
