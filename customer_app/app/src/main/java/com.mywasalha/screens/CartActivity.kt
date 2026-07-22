244f53359e26517381297526ddfce3cf80ae60c6
haldrby28-gif <haldrby28@gmail.com>
2026-07-22 15:39:20 +0300

    Update CartActivity.kt

--- customer_app/app/src/main/java/com.mywasalha/screens/CartActivity.kt
+++ customer_app/app/src/main/java/com.mywasalha/screens/CartActivity.kt
@@ -1,11 +1,10 @@
-
 package com.mywasalha.screens
-
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,88 +12,62 @@ import com.mywasalha.R
 import com.mywasalha.adapters.CartAdapter
 import com.mywasalha.firebase.CartManager
 
-
-
 class CartActivity : AppCompatActivity() {
 
-
     private lateinit var recyclerView: RecyclerView
-
     private lateinit var totalText: TextView
-
     private lateinit var checkoutButton: Button
-
-
     private lateinit var adapter: CartAdapter
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_cart)
 
+        recyclerView = findViewById(R.id.cartRecyclerView)
+        totalText = findViewById(R.id.txtTotal)
+        checkoutButton = findViewById(R.id.btnCheckout)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        recyclerView =
-            findViewById(R.id.cartRecyclerView)
+        adapter = CartAdapter(CartManager.getItems())
+        recyclerView.adapter = adapter
 
-
-        totalText =
-            findViewById(R.id.txtTotal)
-
-
-        checkoutButton =
-            findViewById(R.id.btnCheckout)
-
-
-
-        recyclerView.layoutManager =
-            LinearLayoutManager(this)
-
-
-
-        adapter =
-            CartAdapter(
-                CartManager.getItems()
-            )
-
-
-
-        recyclerView.adapter =
-            adapter
-
-
-
-        updateTotal()
-
-
+        updateCart()
 
         checkoutButton.setOnClickListener {
 
+            if (CartManager.getItems().isEmpty()) {
 
-            startActivity(
-                Intent(
+                Toast.makeText(
                     this,
-                    CheckoutActivity::class.java
+                    "السلة فارغة",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+
+                startActivity(
+                    Intent(
+                        this,
+                        CheckoutActivity::class.java
+                    )
                 )
-            )
 
+            }
         }
-
     }
 
+    override fun onResume() {
+        super.onResume()
+        updateCart()
+    }
 
+    private fun updateCart() {
 
-
-    private fun updateTotal() {
-
+        adapter.notifyDataSetChanged()
 
         totalText.text =
             "الإجمالي: ${CartManager.getTotal()} جنيه"
 
     }
-
 }
