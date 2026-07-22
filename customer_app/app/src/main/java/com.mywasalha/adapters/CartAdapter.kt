23c63b9ac85ae3f033aafbd49e070e3092c5118f
haldrby28-gif <haldrby28@gmail.com>
2026-07-22 03:12:58 +0300

    Update CartAdapter.kt

--- customer_app/app/src/main/java/com.mywasalha/adapters/CartAdapter.kt
+++ customer_app/app/src/main/java/com.mywasalha/adapters/CartAdapter.kt
@@ -1,1 +1,94 @@
+package com.mywasalha.adapters
 
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mywasalha.R
+import com.mywasalha.models.CartItem
+
+
+
+class CartAdapter(
+
+    private val items: MutableList<CartItem>
+
+) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+
+
+
+    class CartViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+
+        val name: TextView =
+            itemView.findViewById(R.id.txtCartProductName)
+
+
+        val quantity: TextView =
+            itemView.findViewById(R.id.txtCartQuantity)
+
+
+        val price: TextView =
+            itemView.findViewById(R.id.txtCartPrice)
+
+    }
+
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CartViewHolder {
+
+
+        val view =
+            LayoutInflater.from(parent.context)
+                .inflate(
+                    R.layout.item_cart,
+                    parent,
+                    false
+                )
+
+
+        return CartViewHolder(view)
+
+    }
+
+
+
+    override fun onBindViewHolder(
+        holder: CartViewHolder,
+        position: Int
+    ) {
+
+
+        val item =
+            items[position]
+
+
+        holder.name.text =
+            item.name
+
+
+        holder.quantity.text =
+            "الكمية: ${item.quantity}"
+
+
+        holder.price.text =
+            "${item.price} جنيه"
+
+
+    }
+
+
+
+    override fun getItemCount(): Int {
+
+        return items.size
+
+    }
+
+}
