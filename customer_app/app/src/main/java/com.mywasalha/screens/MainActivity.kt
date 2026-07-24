7228bf5023721e625acac2a83460afe01543c0d8
haldrby28-gif <haldrby28@gmail.com>
2026-07-25 00:35:19 +0300

    Update MainActivity.kt

--- customer_app/app/src/main/java/com.mywasalha/screens/MainActivity.kt
+++ customer_app/app/src/main/java/com.mywasalha/screens/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.mywasalha.screens
+package com.waselha.customer.screens
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.mywasalha.R
+import com.waselha.customer.R
 
 class MainActivity : AppCompatActivity() {
 
