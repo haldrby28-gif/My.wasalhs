fa8bd30abed61515aa1e7a04ae27f54644aa5449
haldrby28-gif <haldrby28@gmail.com>
2026-07-25 00:40:00 +0300

    Update OrderTrackingActivity.kt

--- customer_app/app/src/main/java/com.mywasalha/screens/OrderTrackingActivity.kt
+++ customer_app/app/src/main/java/com.mywasalha/screens/OrderTrackingActivity.kt
@@ -1,4 +1,4 @@
-package com.mywasalha.screens
+package com.waselha.customer.screens
 
 import android.os.Bundle
 import android.widget.Button
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
-import com.mywasalha.R
+import com.waselha.customer.R
 
 class OrderTrackingActivity : AppCompatActivity() {
 
